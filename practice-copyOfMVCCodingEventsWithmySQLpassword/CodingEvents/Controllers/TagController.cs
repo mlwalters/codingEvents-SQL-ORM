93c3f6d69003a5cff6cd4ca067affc99ba8b8ca0
Maricar Walters <walterskai7@gmail.com>
2021-02-04 11:43:27 -0600

    added Tag model, view, controller

---
+++ practice-copyOfMVCCodingEventsWithmySQLpassword/CodingEvents/Controllers/TagController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodingEvents.Data;
+using CodingEvents.Models;
+using CodingEvents.ViewModels;
+
+namespace CodingEvents.Controllers
+{
+    public class TagController : Controller
+    {
+
+        private EventDbContext context;
+
+        public TagController(EventDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<Tag> tags = context.Tags.ToList();
+            return View(tags);
+        }
+
+        public IActionResult Add()
+        {
+            Tag tag = new Tag();
+            return View(tag);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Tag tag)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Tags.Add(tag);
+                context.SaveChanges();
+                return Redirect("/Tag/");
+            }
+
+            return View("Add", tag);
+        }
+
+    }
+}
---
+++ practice-copyOfMVCCodingEventsWithmySQLpassword/CodingEvents/Models/Tag.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEvents.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 50 characters")]
+        public string Name { get; set; }
+
+        public Tag(string name)
+        {
+            Name = name;
+        }
+
+        public Tag()
+        {
+        }
+    }
+}
